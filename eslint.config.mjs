4157aa9e48aeca1f14858e3c3a4152926c85972a
Erik Nielsen <erik@312development.com>
2026-07-28 08:55:42 -0400

    updated npm dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import jest from "eslint-plugin-jest";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
